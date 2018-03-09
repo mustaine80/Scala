f818fd71843c2b7642a2089205e1697501258e9a
Yongheon Lee <mustaine80@gmail.com>
2018-03-09 22:33:19 +0900

    Either 문제 일부 구현(map, flatMap, orElse)

--- FPinScala/Chapter04.scala
+++ FPinScala/Chapter04.scala
@@ -51,6 +51,101 @@ sealed trait Option[+A] {
   }
 }
 
+object Option {
+  def Try[A](a: => A): Option[A] = {
+    try Some(a)
+    catch { case e: Exception => None }
+  }
+  
+  // 4.3
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    a.flatMap( aa =>
+      b.map(bb =>
+        f(aa, bb) ) )
+  }
+  
+  // 4.4
+  def sequence[A](as: List[Option[A]]): Option[List[A]] = {
+    Try(List.flatMap(as)(o => o match {
+      case Some(v) => List(v)
+      case None => throw new Exception
+    }) )
+  }
+  
+  // 4.5
+  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = {
+    sequence(List.map(as)(f))
+  }
+  
+  def traverse2[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = {
+    Try(List.flatMap(as)(a => f(a) match {
+        case Some(v) => List(v)
+        case None => throw new Exception
+      })
+    )
+  }
+  
+  def sequenceUsingTraverse[A](as: List[Option[A]]): Option[List[A]] = {
+    traverse2(as)(a => a)
+  }
+}
+
+sealed trait Either[+E, +A] {
+  // 4.6
+  def map[B](f: A => B): Either[E, B] = {
+    this match {
+      case Right(r) => Right(f(r))
+      case Left(l) => Left(l):Either[E, B]
+    }
+  }
+  
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = {
+    this match {
+      case Right(r) => f(r)
+      case Left(l) => Left(l):Either[EE, B]
+    }
+  }
+  
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = {
+    this match {
+      case Right(r) => this
+      case Left(l) => b
+    }
+  }
+  
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+    this.flatMap(aa =>
+      b.map(bb =>
+        f(aa,bb) )
+    )  
+  }
+  
+}
+case class Left[+E](value: E) extends Either[E, Nothing]
+case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+  def Try[A](a: => A): Either[Exception, A] = {
+    try Right(a)
+    catch { case e: Exception => Left(e) }
+  }
+  
+  // 4.7
+  /*
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = {
+    val result: Either[Exception, List[A]] = Try( List.flatMap(es)(e => e match {
+      case Right(r) => List(r)
+      case Left(l) => throw new Exception("ERROR!")
+    }) )
+    
+    result
+  }*/
+  /*
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = {
+    
+  }*/
+}
+
 object Chapter04 extends App {
   val o1s = Some(1)
   val o2s = o1s.map(_ * 10)
@@ -159,43 +254,24 @@ object Chapter04 extends App {
   
   println("sequence_traverse(Some) : " + seqs_traverse)
   println("sequence_traverse(None) : " + seqn_traverse)
+  
+  // 4.6
+  val el: Either[String, Int] = Left("error")
+  val er: Either[String, Int] = Right(12345)
+  val el2: Either[String, Int] = Left("fault")
+  val er2: Either[String, Int] = Right(34567)
+  
+  println("map(right): " + er.map(_ * 2))
+  println("map(left): " + el.map(_ * 2))
+  println("flatMap(right->right): " + er.flatMap(r => Right(r*2)) )
+  println("flatMap(right->left): " + er.flatMap(r => Left("none")) )
+  println("flatMap(left->right): " + el.flatMap(l => Right(0)) )
+  println("flatMap(left->left): " + el.flatMap(l => Left("none")) )
+  println("orElse(right): " + er.orElse(er2))
+  println("orElse(left): " + el.orElse(er2))
+  println("map2(right+right): " + er.map2(er2)( (a, b) => a + b))
+  println("map2(right+left): " + er.map2(el2)( (a, b) => a + b))
+  println("map2(left+left): " + el.map2(el2)( (a, b) => a + b))
+  println("map2(left+right): " + el.map2(er2)( (a, b) => a + b))
 }
 
-object Option {
-  def Try[A](a: => A): Option[A] = {
-    try Some(a)
-    catch { case e: Exception => None }
-  }
-  
-  // 4.3
-  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
-    a.flatMap( aa =>
-      b.map(bb =>
-        f(aa, bb) ) )
-  }
-  
-  // 4.4
-  def sequence[A](as: List[Option[A]]): Option[List[A]] = {
-    Try(List.flatMap(as)(o => o match {
-      case Some(v) => List(v)
-      case None => throw new Exception
-    }) )
-  }
-  
-  // 4.5
-  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = {
-    sequence(List.map(as)(f))
-  }
-  
-  def traverse2[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = {
-    Try(List.flatMap(as)(a => f(a) match {
-        case Some(v) => List(v)
-        case None => throw new Exception
-      })
-    )
-  }
-  
-  def sequenceUsingTraverse[A](as: List[Option[A]]): Option[List[A]] = {
-    traverse2(as)(a => a)
-  }
-}
