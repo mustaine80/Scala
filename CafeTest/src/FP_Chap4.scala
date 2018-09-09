d9452301fa3c16fd8900604aa8d9018500df2b51
seok ki <wizard_i@naver.com>
2018-09-10 00:43:30 +0900

    센서 더미 구현에 Option 승급 개념 활용 예제 추가

--- CafeTest/src/FP_Chap4.scala
+++ CafeTest/src/FP_Chap4.scala
@@ -8,6 +8,8 @@ trait ExOption[+A] {
 
   def exFlatMap[B](f:A => ExOption[B]): ExOption[B] = exMap(f) exGetOrElse ExNone
 
+  def exFlatten[B](implicit ev: <:<[A, ExOption[B]]): ExOption[B] = ???
+
   def exGetOrElse[B >: A](default: => B): B = this match {
     case ExSome(a) => a
     case ExNone => default
@@ -28,22 +30,19 @@ case class ExSome[+A](get: A) extends ExOption[A]
 case object ExNone extends ExOption[Nothing]
 
 object Foo {
-  def mean(xs: Seq[Double]): Option[Double] = {
-    if (xs.isEmpty) None
-    else Some(xs.sum / xs.length)
-  }
+  def mean(xs: Seq[Double]): ExOption[Double] =
+    if (xs.isEmpty) ExNone
+    else ExSome(xs.sum / xs.length)
 
-  def variance(xs: Seq[Double]): Option[Double] =
-    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+  def variance(xs: Seq[Double]): ExOption[Double] =
+    mean(xs).exFlatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
 
-  def variance2(xs: Seq[Double]): Option[Double] = {
-    if (xs.isEmpty)
-      None
+  def variance2(xs: Seq[Double]): ExOption[Double] =
+    if (xs.isEmpty) ExNone
     else {
       val m = xs.foldLeft(0.0)(_ + _) / xs.size
-      Some(xs.foldLeft(0.0)((acc, x) => acc + math.pow(x - m, 2)) / xs.size)
+      ExSome(xs.foldLeft(0.0)((acc, x) => acc + math.pow(x - m, 2)) / xs.size)
     }
-  }
 }
 
 case class Employee(name: String, department: String)
@@ -55,6 +54,7 @@ object Bar {
   }
 
   val joeDepartment: Option[String] = lookupByName("Joe").map(_.department)
+
   val dept: String =
     lookupByName("Joe").
       map(_.department).
@@ -65,32 +65,41 @@ object Bar {
     (age + numberOfSpeedingTickets) / 12.0
   }
 
-  def parseInsuranceRateQuote(age: String, numberOfSpeedingTickets: String): Option[Double] = {
-    val optAge: Option[Int] = Try(age.toInt)
-    val optTickets: Option[Int] = Try(numberOfSpeedingTickets.toInt)
+  def parseInsuranceRateQuote(age: String, numberOfSpeedingTickets: String): ExOption[Double] = {
+    val optAge: ExOption[Int] = Try(age.toInt)
+    val optTickets: ExOption[Int] = Try(numberOfSpeedingTickets.toInt)
     map2(optAge, optTickets)(insuranceRateQuote)
   }
 
-  def Try[A](a: => A): Option[A] = {
-    try Some(a)
-    catch { case e: Exception => None }
+  def Try[A](a: => A): ExOption[A] = {
+    try ExSome(a)
+    catch { case e: Exception => ExNone }
   }
 
-  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
-    if (a == None || b == None) None
-    else Some(f(a.get, b.get))
+  def lift[A, B](f: A => B): ExOption[A] => ExOption[B] = _ exMap f
+
+  def map2[A, B, C](a: ExOption[A], b: ExOption[B])(f: (A, B) => C): ExOption[C] = (a, b) match {
+    case (ExNone, _) => ExNone
+    case (_, ExNone) => ExNone
+    case (x: ExSome[A], y: ExSome[B]) => ExSome(f(x.get, y.get))
   }
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
-    case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
-    case _ => Some(List[A]())
+  def sequence[A](a: List[ExOption[A]]): ExOption[List[A]] = a match {
+    case h :: t => h exFlatMap (hh => sequence(t) exMap (hh :: _))
+    case _ => ExSome(List[A]())
   }
 
-  def parseInts(a: List[String]): Option[List[Int]] =
+  def parseInts(a: List[String]): ExOption[List[Int]] =
     sequence(a map (i => Try(i.toInt)))
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = 
+  def traverse[A, B](a: List[A])(f: A => ExOption[B]): ExOption[List[B]] =
     sequence(a map f)
+
+  def traverse2[A, B](a: List[A])(f: A => ExOption[B]): ExOption[List[B]] = a match {
+    case Nil => ExSome(Nil)
+    case h::t => map2(f(h), traverse2(t)(f))(_ :: _)
+  }
+
 }
 
 
@@ -104,6 +113,30 @@ sealed trait ExEither[+E, +A] {
 case class Left[+E](value: E) extends ExEither[E, Nothing]
 case class Right[+A](value: A) extends ExEither[Nothing, A]
 
+//  Todo. 레이더 탐지 모델 구현을 FP 답게 개선해 보자.
+case class Track(id: Int, pos: Double, vel: Double)
+
+trait SurveillanceModel[Track] {
+  def checkAvail(t: Track): Boolean = true
+
+  def checkRange(t: Track): Boolean = true
+
+  def checkDetect(t: Track): Boolean = true
+
+  def updateTrack(t: Track): Track = t
+}
+
+case class RM[Track](scanPeriod: Double, scanRegion: Double, ts: Seq[Track]) extends SurveillanceModel[Track] {
+  def tracking = ts.filter(checkAvail(_)).filter(checkRange(_)).filter(checkDetect(_)).map(updateTrack(_))
+}
+
+case class ECM[Track](scanPeriod: Double, scanRegion: Double, ts: Seq[Track]) extends SurveillanceModel[Track] {
+  def ecmDetect(t: Track): ExOption[Track] = ExSome(t)   // todo. Track type 을 왜 인식하지 못하는걸까?
+
+  //  Option 문맥에서 기존 함수 재활용
+  def tracking = ts.map(ecmDetect(_).exFilter(checkAvail(_)).exFilter(checkRange(_)).exFilter(checkDetect(_)).
+                        exMap(updateTrack(_)).exGetOrElse(new Exception("ECM Tracking fail!")))
+}
 
 
 object FP_Chap4 extends App {
@@ -119,6 +152,7 @@ object FP_Chap4 extends App {
 
   val xs = List(0.0, 2.0, 4.0, 6.0, 8.0)
   println("lists variance => " + Foo.variance(xs))
+  println("lists variance2 => " + Foo.variance2(xs))
 
   println("Joe's department : " + Bar.joeDepartment)
   println("Joe's default dept : " + Bar.dept)
@@ -127,7 +161,14 @@ object FP_Chap4 extends App {
 
   val failLists = List("1", "2", "3", "foo", "5")
   println("StringToInt must fail. => " + Bar.parseInts(failLists) )
-  println("lists traverse => " + Bar.traverse(s)(Some(_)))
+  println("lists traverse => " + Bar.traverse(s)(ExSome(_)))
 
   println("Either map => " + s.map(Right(_).eitherMap(_ + 1)))
+
+  val ts = Seq(Track(1, 1.0, 10.0), Track(2, 2.2, 12.2), Track(3, 3.3, 13.3))
+  val rm = RM(1.5f, 3.14f * 2, ts)
+  rm.tracking foreach(println(_))
+
+  val ecm = ECM(3.0f, 3.14f, ts)
+  ecm.tracking foreach((println(_)))
 }
